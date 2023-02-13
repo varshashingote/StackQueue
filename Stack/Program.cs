b26bff5368d11db86b3ef9b9a68557a9d7e84ec3
varsha Gunjal <varshashingote01@gmail.com>
2023-02-13 15:43:56 +0530

    [varsha] Queue added

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,38 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.push(56);
-            stack.push(30);
-            stack.push(70); 
-            stack.peak();
-            stack.pop();
-            stack.peak();
-            stack.pop();
-            stack.peak();
-            stack.pop();
-            stack.display();
-           Console.ReadLine();
-
+            Console.WriteLine("Enter Your choice 1.stack\n2.queue");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Stack stack = new Stack();
+                    stack.push(56);
+                    stack.push(30);
+                    stack.push(70);
+                    stack.peak();
+                    stack.pop();
+                    stack.peak();
+                    stack.pop();
+                    stack.peak();
+                    stack.pop();
+                    stack.display();
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Queue queue = new Queue();
+                    queue.Enqueue(30);
+                    queue.Enqueue(70);
+                    queue.display();
+                    queue.Dequeue();
+                    queue.display();
+                    queue.Dequeue();
+                    queue.display();
+                    queue.Dequeue();
+                    queue.display(); ;
+                    Console.ReadLine();
+                    break;
+                }
         }
     }
 }
---
+++ Stack/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Queue
+    {
+            Node front;
+            Node rear;
+            public void Enqueue(int data)
+            {
+                Node newNode = new Node(data);
+                if (this.rear == null)
+                {
+                    this.front = this.rear = newNode;
+                }
+                else
+                {
+                    this.rear.next = newNode;
+                    this.rear = newNode;
+                }
+                Console.WriteLine($"{data} Inserted into Queue");
+            }
+            public void Dequeue()
+            {
+                if (this.front == null)
+                {
+                    Console.WriteLine(" Queue is Empty");
+                    return;
+                }
+                Node temp = this.front;
+                this.front = this.front.next;
+
+                if (this.front == null)
+                {
+                    this.rear = null;
+                }
+
+                Console.WriteLine("Element deleted is {0}", temp.data);
+            }
+            public void display()
+            {
+            int i = 1;
+
+            Node temp = front;
+            if (temp == null)
+            {
+                Console.WriteLine(" Queue is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($" Element {i} in the Queue is : " + temp.data);
+                temp = temp.next;
+
+
+            }
+
+        }
+    }
+}
+
+
+
+    
+
