ba454b50b452f508a2824e504ef72adfbacf3699
varsha Gunjal <varshashingote01@gmail.com>
2023-02-13 15:23:22 +0530

    [varsha]Added Stack Elemen

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(56);
+            stack.push(30);
+            stack.push(70);
+            stack.display();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public Node top;
+
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine(" Pushed to Stack is :" + data);
+        }
+
+        public void display()
+        {
+            int i = 1;
+           
+            Node temp = top;
+            if (temp == null)
+            {
+                Console.WriteLine(" Stack is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($" Element {i} in the Stack is : " + temp.data);
+                temp = temp.next;
+                
+                
+            }
+           
+        }
+    }
+}
